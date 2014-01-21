767f85438f31752a65d97e3215f0ee10cc40764d
pawanadh <pawan.adh@gmail.com>
2014-01-21 03:50:36 -0800

    Create disable_enable_index.sql
    SQL Server/TSQL to disable or enable index in the tables

---
+++ disable_enable_index.sql
@@ -0,0 +1,10 @@
+
+--How to disable & enable index in the table?
+--Use the following query to disable or enable index in the tables.
+--Disable Index
+ALTER INDEX [IndexName] ON TableName DISABLE
+GO
+--Enable Index
+ALTER INDEX [IndexName] ON TableName REBUILD
+GO
+
